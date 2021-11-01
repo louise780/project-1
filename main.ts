ee34659982c35d8f93b5ae68354a987d5e1316fb
louise780 <91077909+louise780@users.noreply.github.com>
2021-11-01 14:31:09 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (images.createImage(`
+        . . . . .
+        # . # . #
+        . # # # .
+        . . # . .
+        . # # # .
+        `)))
+})
+basic.forever(function () {
+	
+})
